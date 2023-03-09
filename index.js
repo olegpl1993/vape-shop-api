30157fe87255ad75f305560bf4a2a8df9ae222e0
olegpl1993 <olegpl1993@gmail.com>
2023-03-09 21:55:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.status(200).json('Server is working!');
+});
+
+app.listen(PORT, console.log("server started on port " + PORT));
