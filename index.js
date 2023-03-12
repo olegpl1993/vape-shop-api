a9d3fe7d84d36154297b94525b9231e998f1c5fa
olegpl1993 <olegpl1993@gmail.com>
2023-03-13 00:21:47 +0200

    feat: add cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import router from './router.js';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 5000;
 const BD_URL = 'mongodb+srv://user:user@cluster0.kpncn2z.mongodb.net/?retryWrites=true&w=majority';
 const app = express();
 
-app.use(express.json());
-app.use(express.static('static'));
-app.use('/api', router);
+app.use(cors()); // подключение корс заголовков
+app.use(express.json()); // позволяет Exprees преобразовывать JSON формат
+app.use(express.static('static')); // путь к папке со статическим содержимым (картинки и ТД)
+app.use('/api', router); // подключение роутера
 
 async function startApp() {
   try {
